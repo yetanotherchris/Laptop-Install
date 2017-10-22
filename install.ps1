b9048ff6fe37862c96fb92673e77424ce1171353
Chris S <chrisasmall+github@gmail.com>
2017-10-22 11:08:30 +0100

    Update install.ps1
    check for "yes" for vs2017 installation

--- install.ps1
+++ install.ps1
@@ -13,7 +13,7 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 choco feature enable -n allowGlobalConfirmation
 
 # For Hyper-V on the desklaptop
-if ($env:INSTALL_VS2017 -ne "")
+if ($env:INSTALL_VS2017 -eq "yes")
 {
     choco install visualstudio2017community
     choco install visualstudio2017-workload-netcoretools
